1e8865aec35f045f1fe46fbdbe6b669a58cef31c
Darko Gjorgjijoski <dg@darkog.com>
2019-04-13 22:27:18 +0000

    Added WebP support

---
+++ src/Responses/ResultWebP.php
@@ -0,0 +1,37 @@
+<?php
+/********************************************************************
+ * Copyright (C) 2018 MegaOptim (https://megaoptim.com)
+ *
+ * This file is part of MegaOptim Image Optimizer
+ *
+ * MegaOptim Image Optimizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MegaOptim Image Optimizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MegaOptim Image Optimizer. If not, see <https://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+namespace MegaOptim\Responses;
+
+class ResultWebP {
+	public $url;
+	public $optimized_size;
+	public $saved_bytes;
+	public $saved_percent;
+
+	public function __construct($response) {
+		if(isset($response->webp) && isset($response->webp->url)) {
+			$this->url = $response->webp->url;
+			$this->optimized_size = $response->webp->optimized_size;
+			$this->saved_bytes = $response->webp->saved_bytes;
+			$this->saved_percent = $response->webp->saved_percent;
+		}
+	}
+}
